2f551df677f7f4f5408ebe65a654f2aff8e5bd91
Dylan Williams <happyfrog578+github@gmail.com>
2020-07-02 23:58:33 +1000

    add array randomiser for testing

--- main.rb
+++ main.rb
@@ -3,22 +3,8 @@
 require_relative 'node'
 require_relative 'tree'
 
-test_arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-test_arr2 = [2, 1, 3, 4, 4, 4, 4]
-test_arr3 = [3, 2, 7, 1, 5, 4, 6]
+test_arr = Array.new(15) { rand(1..100) }
 
-tree = Tree.new(test_arr2)
-
-tree.pretty_print
-
-tree.insert(500)
-
-puts '----------'
-
-tree.pretty_print
-
-puts '----------'
-
-tree.insert(99)
+tree = Tree.new(test_arr)
 
 tree.pretty_print
