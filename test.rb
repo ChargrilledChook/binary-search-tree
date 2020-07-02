00c6f35f90feec88f788cdbceb00c9a9365f5b6c
chook <dylanw212@gmail.com>
2020-07-02 19:20:48 +1000

    build tree experiments

--- test.rb
+++ test.rb
@@ -26,3 +26,23 @@ def build_tree(array)
   end
   root
 end
+
+def node_placer(temp_root)
+  if element < temp_root.data
+    if temp_root.left.nil?
+      temp_root.left = Node.new(element)
+      true
+    else
+      temp_root = temp_root.left
+      temp_root
+    end
+  else
+    if temp_root.right.nil?
+      temp_root.right = Node.new(element)
+      true
+    else
+      temp_root = temp_root.right
+      temp_root
+    end
+  end
+end
